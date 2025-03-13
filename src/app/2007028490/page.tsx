01b5f8882705134e9a3a27bbce65dd93d722e0b3
Lobinda <lobinda123@gmail.com>
2025-03-14 07:01:25 +0800

    sc

--- src/app/2007028490/page.tsx
+++ src/app/2007028490/page.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import ProfileClient from "@/components/ProfileClient";
-import Cookies from "js-cookie";
-interface PageProps {
-  params: { id: string }; // ✅ 正確的 `params` 型別
-}
 
-export default function DynamicPage({ params }: PageProps) {
-  const { id } = params; // ✅ 直接讀取 URL 參數
+
+import Cookies from "js-cookie";
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const Page = ({ params }: PageProps) => {
+  useEffect(() => {
+    const { id } = params; // ✅ 直接讀取 URL 參數
   Cookies.set("channelId", id, { expires: 7 });
-  return <ProfileClient />;
-}
+  }, [params]);
+
+  return (
+    return <ProfileClient />;
+  );
+};
+
+
+
+
