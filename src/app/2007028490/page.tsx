810aca2458acc52f032f80d73dabcb669af41e0e
Lobinda <lobinda123@gmail.com>
2025-03-14 07:29:50 +0800

    8585

--- src/app/2007028490/page.tsx
+++ src/app/2007028490/page.tsx
@@ -1,5 +1,5 @@
-import ProfileClient from "@/components/ProfileClient";
 import Cookies from "js-cookie";
+import ProfileClient from "./_components/ProfileClient";
 
 export default function DynamicPage() {
   Cookies.set("channelId", "2007028490", { expires: 7 });
