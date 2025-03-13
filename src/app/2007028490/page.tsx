0502a7544db0eb77e0af2f3f9725a8d9b88be80b
Lobinda <lobinda123@gmail.com>
2025-03-14 07:02:10 +0800

    99

--- src/app/2007028490/page.tsx
+++ src/app/2007028490/page.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import ProfileClient from "@/components/ProfileClient";
 
-
 import Cookies from "js-cookie";
 type PageProps = {
   params: {
@@ -12,14 +11,8 @@ type PageProps = {
 const Page = ({ params }: PageProps) => {
   useEffect(() => {
     const { id } = params; // ✅ 直接讀取 URL 參數
-  Cookies.set("channelId", id, { expires: 7 });
+    Cookies.set("channelId", id, { expires: 7 });
   }, [params]);
 
-  return (
-    return <ProfileClient />;
-  );
+  return <ProfileClient />;
 };
-
-
-
-
