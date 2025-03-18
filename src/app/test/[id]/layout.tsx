2fbda04a2c70a976487da2016a922c2221262da3
Lobinda <lobinda123@gmail.com>
2025-03-18 10:22:31 +0800

    xxx

--- src/app/test/[id]/layout.tsx
+++ src/app/test/[id]/layout.tsx
@@ -1,11 +1,13 @@
 import LiffProvider from "@/app/_components/LiffProvider/LiffProvider";
-import { PropsWithChildren } from "react";
+import { ReactNode } from "react";
 
-export default function IdLayout({
-  children,
-  params,
-}: PropsWithChildren<{ params: { id: string } }>) {
-  // 根據 id 動態取得 LIFF ID
+interface LayoutProps {
+  children: ReactNode;
+  params: { id: string }; // 確保 params 是正確的型別
+}
+
+export default function IdLayout({ children, params }: LayoutProps) {
+  // 根據 id 取得對應的 LIFF ID
   const dynamicLiffId = getLiffIdFromDynamicParam(params.id);
 
   return <LiffProvider liffId={dynamicLiffId}>{children}</LiffProvider>;
@@ -14,8 +16,8 @@ export default function IdLayout({
 // 根據 id 回傳對應的 LIFF ID（可改為 API 查詢）
 function getLiffIdFromDynamicParam(id: string): string {
   const liffIds: Record<string, string> = {
-    2007028490: "2007049862-Le590xkP",
-    2007054553: "LIFF_ID_2",
+    shopA: "LIFF_ID_1",
+    shopB: "LIFF_ID_2",
   };
 
   return liffIds[id] || "DEFAULT_LIFF_ID";
