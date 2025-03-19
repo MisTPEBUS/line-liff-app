7e97bbdfdb4ce76bde9cdbcb0dc29cd26f4dabd2
Lobinda <lobinda123@gmail.com>
2025-03-19 11:52:57 +0800

    update

--- src/app/cashNotify/[id]/page.tsx
+++ src/app/cashNotify/[id]/page.tsx
@@ -2,13 +2,13 @@
 
 import ProductDetail from "@/app/_components/ProductDetail/ProductDetail";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+/* import { useEffect, useState } from "react"; */
 
 const CashNotifyPage = () => {
   const params = useParams();
   const id = typeof params.id === "string" ? params.id : params.id?.[0] || "";
 
-  const [data, setData] = useState<any>(null);
+  /*   const [data, setData] = useState<any>(null); */
 
   return (
     <div className="p-6">
